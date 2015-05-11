bc140acbc66e294062d62371b5a2ea268ab4df8c
Yury Voloshin <yvoloshin01@gmail.com>
2015-05-11 13:58:11 +0000

    Lesson 14

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,11 +4,14 @@ class QuotesController < ApplicationController
 	end
 
 	def new
-		@quote=Quote.new
+		@quote = Quote.new
 	end
 
 	def create
-		Quote.create(quote_params)
+		@quote = Quote.create(quote_params)
+		if @quote.invalid?
+			flash[:error]='<strong>Could not save</strong> the data you entered is invalid.'
+		end
 		redirect_to root_path
 	end
 
