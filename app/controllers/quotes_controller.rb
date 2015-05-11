b0decaad7f86b17ba2b3f2b0548c725740ce897b
Yury Voloshin <yvoloshin01@gmail.com>
2015-05-11 18:12:00 +0000

    Lesson 16

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,11 +2,7 @@ class QuotesController < ApplicationController
 	def index
 		@quote = Quote.order("RANDOM()").first
 	end
-
-	def new
-		@quote = Quote.new
-	end
-
+	
 	def create
 		@quote = Quote.create(quote_params)
 		if @quote.invalid?
@@ -15,9 +11,14 @@ class QuotesController < ApplicationController
 		redirect_to root_path
 	end
 
+	def about		
+	end
+
 	private
 
 	def quote_params
 		params.require(:quote).permit(:saying, :author)
 	end
+
+
 end
