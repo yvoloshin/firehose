2bf5b09c2fe6e9c38b5b580d72a2ff9ca9fd7aaa
Yury Voloshin <yvoloshin01@gmail.com>
2015-06-05 06:27:31 +0000

    TDD 4

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -14,6 +14,13 @@ class QuotesController < ApplicationController
 	def about		
 	end
 
+	def show
+		@quote = Quote.where(:id => params[:id]).first
+		if @quote.blank?
+			render :text => "not found", :status => :not_found
+		end
+	end
+
 	private
 
 	def quote_params
